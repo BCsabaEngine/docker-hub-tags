cb87feda681c39544f4e9c8f3ccb0abdcf551b5d
Balázs Csaba <csaba@balazs-magyar.hu>
2023-12-23 13:52:03 +0100

    fix: format

--- test/parseVersionWithLevel.test.ts
+++ test/parseVersionWithLevel.test.ts
@@ -4,21 +4,21 @@ import { ParsedVersionLevel } from '../src/types';
 import { parseVersionWithLevel } from '../src/utils';
 
 const tests: Record<string, Partial<ParsedVersionLevel>> = {
-    '1.2.3': { major: 1, minor: 2, patch: 3, postfix: undefined, semverLevel: 'patch' },
-    '1.2': { major: 1, minor: 2, patch: 0, postfix: undefined, semverLevel: 'minor' },
-    '1': { major: 1, minor: 0, patch: 0, postfix: undefined, semverLevel: 'major' },
-    '~1': { major: 1, minor: 0, patch: 0, postfix: undefined, semverLevel: 'patch' },
-    '^1': { major: 1, minor: 0, patch: 0, postfix: undefined, semverLevel: 'minor' },
-    $1: { major: 1, minor: 0, patch: 0, postfix: undefined, semverLevel: 'evenMajor' },
-    'v1.2.3': { major: 1, minor: 2, patch: 3, postfix: undefined, semverLevel: 'patch' },
-    'v1.2': { major: 1, minor: 2, patch: 0, postfix: undefined, semverLevel: 'minor' },
-    v1: { major: 1, minor: 0, patch: 0, postfix: undefined, semverLevel: 'major' },
-    '~v1': { major: 1, minor: 0, patch: 0, postfix: undefined, semverLevel: 'patch' },
-    '^v1': { major: 1, minor: 0, patch: 0, postfix: undefined, semverLevel: 'minor' },
-    $v1: { major: 1, minor: 0, patch: 0, postfix: undefined, semverLevel: 'evenMajor' }
+	'1.2.3': { major: 1, minor: 2, patch: 3, postfix: undefined, semverLevel: 'patch' },
+	'1.2': { major: 1, minor: 2, patch: 0, postfix: undefined, semverLevel: 'minor' },
+	'1': { major: 1, minor: 0, patch: 0, postfix: undefined, semverLevel: 'major' },
+	'~1': { major: 1, minor: 0, patch: 0, postfix: undefined, semverLevel: 'patch' },
+	'^1': { major: 1, minor: 0, patch: 0, postfix: undefined, semverLevel: 'minor' },
+	$1: { major: 1, minor: 0, patch: 0, postfix: undefined, semverLevel: 'evenMajor' },
+	'v1.2.3': { major: 1, minor: 2, patch: 3, postfix: undefined, semverLevel: 'patch' },
+	'v1.2': { major: 1, minor: 2, patch: 0, postfix: undefined, semverLevel: 'minor' },
+	v1: { major: 1, minor: 0, patch: 0, postfix: undefined, semverLevel: 'major' },
+	'~v1': { major: 1, minor: 0, patch: 0, postfix: undefined, semverLevel: 'patch' },
+	'^v1': { major: 1, minor: 0, patch: 0, postfix: undefined, semverLevel: 'minor' },
+	$v1: { major: 1, minor: 0, patch: 0, postfix: undefined, semverLevel: 'evenMajor' }
 };
 
 describe('parseVersion', () => {
-    for (const [input, output] of Object.entries(tests))
-        test(input, () => expect(parseVersionWithLevel(input)).toMatchObject(output));
+	for (const [input, output] of Object.entries(tests))
+		test(input, () => expect(parseVersionWithLevel(input)).toMatchObject(output));
 });
